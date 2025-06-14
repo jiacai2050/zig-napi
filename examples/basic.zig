127f2bee47b41d7ae7869aa5c4cd86384397b788
Jiacai Liu <dev@liujiacai.net>
2025-06-14 21:50:01 +0800

    feat: support for scope (#4)
    * feat: support for scope
    
    * fix ci
    
    * docs

--- examples/basic.zig
+++ examples/basic.zig
@@ -26,6 +26,17 @@ fn add(e: napi.Env, n1: napi.Value, n2: napi.Value) !napi.Value {
     return try e.createDouble(d1 + d2);
 }
 
+fn scope_demo(e: napi.Env) !napi.Value {
+    for (0..100000) |i| {
+        const scope = try e.openScope();
+        defer scope.deinit() catch unreachable;
+        const value = try e.createDouble(@floatFromInt(i));
+        // Do something with value, and it will get freed when scope is deinited.
+        _ = value;
+    }
+    return null;
+}
+
 pub fn init(env: napi.Env, exports: napi.Value) !napi.Value {
     try env.setNamedProperty(
         exports,
@@ -45,5 +56,10 @@ pub fn init(env: napi.Env, exports: napi.Value) !napi.Value {
         try env.createFunction("add", add),
     );
 
+    try env.setNamedProperty(
+        exports,
+        "scope_demo",
+        try env.createFunction("scope_demo", scope_demo),
+    );
     return exports;
 }
