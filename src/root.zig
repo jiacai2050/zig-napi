58f7cd1a3dba201ec45a79e433b478599ab10472
Jiacai Liu <dev@liujiacai.net>
2025-06-18 21:36:46 +0800

    refactor: add dedicated `Value` type to wrap `c.napi_value` (#5)
    * refactor: add Value type
    
    * fix cr

--- src/root.zig
+++ src/root.zig
@@ -1,187 +1,45 @@
 const std = @import("std");
 const c = @import("c.zig").c;
 const callNodeApi = @import("c.zig").callNodeApi;
-pub const Value = @import("c.zig").Value;
+pub const Value = @import("Value.zig");
+const util = @import("util.zig");
 
 /// Env is a wrapper around the Node-API environment handle (`napi_env`).
 /// It provides methods to interact with the Node-API, such as type conversions between Zig and JavaScript values.
 pub const Env = struct {
     c_handle: c.napi_env,
 
-    pub fn getValueDouble(self: Env, value: Value) !f64 {
-        var result: f64 = undefined;
-        try callNodeApi(
-            self.c_handle,
-            c.napi_get_value_double,
-            .{ value, &result },
-        );
-        return result;
+    pub fn create(self: Env, comptime T: type, value: T) !Value {
+        return try Value.try_from(T, self, value);
     }
 
-    pub fn getValueInt64(self: Env, value: Value) !i64 {
-        var result: i64 = undefined;
-        try callNodeApi(
-            self.c_handle,
-            c.napi_get_value_int64,
-            .{ value, &result },
-        );
-        return result;
+    pub fn createString(
+        self: Env,
+        comptime encoding: Value.StringEncoding,
+        str: if (encoding == .utf16) []const u16 else []const u8,
+    ) !Value {
+        return try Value.createString(self, encoding, str);
     }
 
-    pub fn getValueUint32(self: Env, value: Value) !u32 {
-        var result: u32 = undefined;
-        try callNodeApi(
-            self.c_handle,
-            c.napi_get_value_uint32,
-            .{ value, &result },
-        );
-        return result;
+    pub fn createObject(
+        self: Env,
+    ) !Value {
+        return try Value.createObject(self);
     }
 
-    pub fn getValueInt32(self: Env, value: Value) !i32 {
-        var result: i32 = undefined;
-        try callNodeApi(
-            self.c_handle,
-            c.napi_get_value_int32,
-            .{ value, &result },
-        );
-        return result;
-    }
-
-    pub fn getValueStringUtf8(self: Env, value: Value, out_str: []u8) !usize {
-        // Number of bytes copied into the buffer, excluding the null terminator.
-        var len: usize = 0;
-        try callNodeApi(
-            self.c_handle,
-            c.napi_get_value_string_utf8,
-            .{ value, out_str.ptr, out_str.len, &len },
-        );
-        return len;
-    }
-
-    pub fn createDouble(self: Env, num: f64) !Value {
-        var result: Value = undefined;
-        try callNodeApi(
-            self.c_handle,
-            c.napi_create_double,
-            .{ num, &result },
-        );
-        return result;
-    }
-
-    pub fn createInt64(self: Env, num: i64) !Value {
-        var result: Value = undefined;
-        try callNodeApi(
-            self.c_handle,
-            c.napi_create_int64,
-            .{ num, &result },
-        );
-        return result;
-    }
-
-    pub fn createUint32(self: Env, num: u32) !Value {
-        var result: Value = undefined;
-        try callNodeApi(
-            self.c_handle,
-            c.napi_create_uint32,
-            .{ num, &result },
-        );
-        return result;
-    }
-
-    pub fn createInt32(self: Env, num: i32) !Value {
-        var result: Value = undefined;
-        try callNodeApi(
-            self.c_handle,
-            c.napi_create_int32,
-            .{ num, &result },
-        );
-        return result;
-    }
-
-    /// Creates a JavaScript string value from a UTF8-encoded slice.
-    /// The native string is copied.
-    pub fn createStringUtf8(self: Env, str: []const u8) !Value {
-        var result: Value = undefined;
-        try callNodeApi(
-            self.c_handle,
-            c.napi_create_string_utf8,
-            .{ str.ptr, str.len, &result },
-        );
-        return result;
-    }
-
-    pub fn createObject(self: Env) !Value {
-        var result: Value = undefined;
-        try callNodeApi(
-            self.c_handle,
-            c.napi_create_object,
-            .{&result},
-        );
-        return result;
-    }
-
-    pub fn createFunction(self: Env, name: []const u8, func: anytype) !Value {
-        const fn_info = switch (@typeInfo(@TypeOf(func))) {
-            .@"fn" => |fn_info| fn_info,
-            else => @compileError("`func` must be a function"),
-        };
-        if (fn_info.params.len == 0) @compileError("Function requires at least one parameter.");
-        if (fn_info.params[0].type != Env) @compileError("The first parameter of function must be `Env`.");
-        inline for (fn_info.params[1..]) |param| {
-            if (param.type != Value) @compileError("The rest parameters of function must be of type `Value`.");
-        }
-        const num_params = fn_info.params.len - 1;
-
-        var function: c.napi_value = undefined;
-        try callNodeApi(
-            self.c_handle,
-            c.napi_create_function,
-            .{
-                name.ptr,
-                name.len,
-                struct {
-                    fn callback(env: c.napi_env, info: c.napi_callback_info) callconv(.C) Value {
-                        var argc: usize = num_params;
-                        var argv: [num_params]Value = undefined;
-                        callNodeApi(
-                            env,
-                            c.napi_get_cb_info,
-                            .{ info, &argc, &argv, null, null },
-                        ) catch |err| {
-                            _ = c.napi_throw_error(env, null, @errorName(err));
-                            return null;
-                        };
-                        if (argc != num_params) {
-                            _ = c.napi_throw_error(env, null, std.fmt.comptimePrint("Incorrect number of arguments, expected {d}", .{num_params}));
-                            return null;
-                        }
-                        var full_args: std.meta.ArgsTuple(@TypeOf(func)) = undefined;
-                        full_args[0] = Env{ .c_handle = env };
-                        inline for (argv, 1..) |arg, i| {
-                            full_args[i] = arg;
-                        }
-                        return if (comptime isReturnValue(fn_info))
-                            @call(.auto, func, full_args)
-                        else
-                            @call(.auto, func, full_args) catch |err| {
-                                _ = c.napi_throw_error(env, null, @errorName(err));
-                                return null;
-                            };
-                    }
-                }.callback,
-                null,
-                &function,
-            },
-        );
-        return function;
+    pub fn createFunction(
+        self: Env,
+        func: anytype,
+        comptime name: ?[]const u8,
+    ) !Value {
+        return try Value.createFunction(self, func, name);
     }
 
     pub fn setNamedProperty(self: Env, exports: Value, name: [:0]const u8, prop: Value) !void {
         try callNodeApi(
             self.c_handle,
             c.napi_set_named_property,
-            .{ exports, name.ptr, prop },
+            .{ exports.c_handle, name.ptr, prop.c_handle },
         );
     }
 
@@ -205,21 +63,24 @@ pub const Env = struct {
 /// It can return `Value` or `!Value`.
 pub fn registerModule(init_fn: anytype) void {
     const Closure = struct {
-        fn init(env: c.napi_env, exports: c.napi_value) callconv(.C) Value {
+        fn init(c_env: c.napi_env, c_exports: c.napi_value) callconv(.C) c.napi_value {
             const fn_info = switch (@typeInfo(@TypeOf(init_fn))) {
                 .@"fn" => |fn_info| fn_info,
                 else => @compileError("`init_fn` must be a function"),
             };
             if (!(fn_info.params.len == 2 and fn_info.params[0].type == Env and fn_info.params[1].type == Value)) @compileError("`init` function requires two arguments: (Env, Value).");
 
-            if (comptime isReturnValue(fn_info)) {
-                return init_fn(Env{ .c_handle = env }, exports);
-            } else if (comptime isReturnErrValue(fn_info)) {
-                return init_fn(Env{ .c_handle = env }, exports) catch |e| {
+            const env = Env{ .c_handle = c_env };
+            const exports = Value{ .env = env, .c_handle = c_exports };
+            if (comptime util.isReturnValue(fn_info)) {
+                return init_fn(env, exports).c_handle;
+            } else if (comptime util.isReturnErrValue(fn_info)) {
+                const ret = init_fn(env, exports) catch |e| {
                     std.log.err("Init zig-napi failed, err: {any}", .{e});
-                    _ = c.napi_throw_error(env, null, @errorName(e));
+                    _ = c.napi_throw_error(c_env, null, @errorName(e));
                     return null;
                 };
+                return ret.c_handle;
             } else {
                 @compileError("`init` function must return `Value` or `!Value` type");
             }
@@ -254,23 +115,4 @@ fn scope(comptime escape: bool) type {
             );
         }
     };
-}
-
-fn isReturnValue(fn_info: std.builtin.Type.Fn) bool {
-    if (fn_info.return_type) |ret_type| {
-        return ret_type == Value;
-    }
-
-    return false;
-}
-
-fn isReturnErrValue(fn_info: std.builtin.Type.Fn) bool {
-    if (fn_info.return_type) |ret_type| {
-        switch (@typeInfo(ret_type)) {
-            .error_union => |err_union| return err_union.payload == Value,
-            else => {},
-        }
-    }
-
-    return false;
 }
